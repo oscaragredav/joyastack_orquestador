6b91efe242022de8529e99d191ac5df472ca729b
AlejandroHancco <142966841+AlejandroHancco@users.noreply.github.com>
2025-10-13 09:15:34 -0500

    Update config.py

--- db/config.py
+++ db/config.py
@@ -3,3 +3,7 @@ DB_PASSWORD = "joya123"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_NAME = "joyastack_db"
+
+SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
